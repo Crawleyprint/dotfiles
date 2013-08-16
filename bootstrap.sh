80503084f9b60af8fb38e3073f588c5e994fa328
Mladen Stepanic <mladen.stepanic@gmail.com>
2013-08-16 10:32:38 +0200

    Adds bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+home_dir=/Users/mladen/
+#set dotfiles repo location
+target_dir=/Users/mladen/repos/dotfiles
+
+#and cd there
+cd $target_dir
+
+#update submodules to enable Vundle
+git submodule update --init &&
+
+cd
+
+#loop through files and link them
+for file in vimrc vim tmux.conf teamocil ackrc gitconfig
+do
+  echo "Linking $file."
+  ln -s $target_dir/$file .$file
+done
+
+#run vim for the first time and install bundles
+vim +BundleInstall +qall
